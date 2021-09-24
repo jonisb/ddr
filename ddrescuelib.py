b4f530ca5c028b2d35d8f2e11d1e00fbd153757a
jonisb <github.com@JsBComputing.se>
2021-09-24 03:32:47 +0200

    Update ddrescuelib.py
    Adds prototype output method

--- ddrescuelib.py
+++ ddrescuelib.py
@@ -3,7 +3,6 @@ from pathlib import Path
 import re as regex
 
 
-
 class ddrescue_class:
     bin = Path('ddrescue')
 
@@ -27,3 +26,42 @@ class ddrescue_class:
                 if result:
                     self.version = result.groups()[0]
                     return self.version
+
+    def output(self, inpath, outpath):
+        settings = ['--max-read-rate=102400', '--sparse', '--no-trim', '--max-read-errors=0', '--min-read-rate=1Mi', '--max-slow-reads=1']
+        command = [
+            self.bin,
+            *settings,
+            inpath,
+            outpath,
+            outpath.with_suffix(outpath.suffix + '.log')
+            ]
+        with self.run_ddrescue(command) as proc:
+            self.values['status'] = tuple()
+            while proc.poll() is None:
+                line = proc.stdout.readline().strip().replace('[A', '')
+                if line == '':
+                    continue
+                result = regex.findall(r"\s*(\w[\w\s-]+):\s+(.*?)(?:,|$)", line)
+                if result:
+                    for res in result:
+                        self.values[res[0]] = res[1].strip()
+                        if 'time since last successful read' == res[0]:
+                            yield self.values['pct rescued']
+                else:
+                    result = regex.match(r"(Copying non-tried blocks\.\.\.) Pass (\d+) \((forwards)\)", line)
+                    if result:
+                        self.values['status'] = result.group()
+                    else:
+                        if line == "Interrupted by user":
+                            self.values['status'] = line
+                        elif line == "Finished":
+                            self.values['status'] = line
+                        elif line == "GNU ddrescue 1.25":
+                            pass
+                        elif line == "Press Ctrl-C to interrupt":
+                            pass
+                        else:
+                            print('not:', repr(line))
+            else:
+                yield self.values['pct rescued']
